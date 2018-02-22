8c63c88d356cf7d1c4dacb0954e8b037f79ef082
ElisaZornig <elisazornig@gmail.com>
2018-02-22 20:47:01 +0100

    Unity game 1

---
+++ untiy ding/Assets/Collectibles/scripts/Spinning3.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinning3 : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(new Vector3(0, 0, 0), new Vector3 (0, 90, 0), 20 * Time.deltaTime);
+    }
+}
